bb9befc87f73a8a57672b9c6e481eaaaa27291d3
ganeshprasad82 <33683462+ganeshprasad82@users.noreply.github.com>
2020-07-24 16:08:37 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,39 +1,43 @@
 #!/bin/bash -x 
+
+echo -e "\n\nPWD is:  $PWD"
+
+echo -e "\n\nContent of this folder:$PWD"
+ls -lrt 
+
+
 $CC my-hello.c -o my-hello.out
 $CC my-hello.c -o a.out
 
-echo "Content of environment variables"
+echo -e "\n\nContent of environment variables"
+echo "F90:$F90"
+echo "FC:$FC"
+echo "F77:$F77"
+echo "CC:$CC"
 echo "PATH:$PATH"
 echo "LD_LIBRARY_PATH:$LD_LIBRARY_PATH"
 echo "LIBRARY_PATH:$LIBRARY_PATH"
 echo "C_INCLUDE_PATH:$C_INCLUDE_PATH"
 echo "CXX_INCLUDE_PATH:$CXX_INCLUDE_PATH"
 echo "CPP_INCLUDE_PATH:$CPP_INCLUDE_PATH"
-echo "F90:$F90"
-echo "FC:$FC"
-echo "F77:$F77"
-echo "CC:$CC"
+echo "RPATH:$RPATH"
+echo "RUNPATH:$RUNPATH"
+echo "LD_RUN_PATH:$LD_RUN_PATH"
 
-echo "Location information"
+echo -e "\n\nWhich compilers are present:"
 which $CC
 which clang 
-which gcc 
+which clang++ 
 which flang
-echo "I am in $PWD"
+which gcc 
+which g++
+which gfortran
 
 
-echo "Version information"
+echo -e "\n\nVersion information"
 clang -v 
 flang -v
 gcc -v 
 
-
-echo "Content of this folder:$PWD"
-ls -lrt 
-
-echo "ganesh debug statements"
-echo "RPATH:$RPATH"
-echo "RUNPATH:$RUNPATH"
-
-icc -v
-which icc
+echo -e "\n\n Hello world programs check"
+bash clang_hello_world.sh
